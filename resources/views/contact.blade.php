ea3fd85d28f8713e0a5da6dab2d112fc07e756d5
Travel-Agent7 <127250295+Travel-Agent7@users.noreply.github.com>
2024-08-16 20:46:40 +0400

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,7 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet">
-    <style>
+<!--     <style>
         :root {
             --theme-color: {{ env('THEME_COLOR', '#000000') }};
         }
@@ -16,16 +16,16 @@
         body {
             background-color: var(--theme-color);
         }
-    </style>
+    </style> -->
 </head>
 
-<body>
+<body style="background-image: url('{{ asset('admin/assets/images/img1.jpg') }}'); background-repeat: no-repeat; background-size: cover;">
     <section class="ftco-section py-5 mt-4 ml-2">
         <div class="container d-flex justify-content-center">
             <div class="row justify-content-center">
                 <div class="col-lg-10">
                     <div class="wrapper img"
-                        style="background-image: url('{{ asset('admin/assets/images/img.jpg') }}');">
+                        style="background-image: url('{{ asset('admin/assets/images/img7.jpg') }}');">
                         <div class="row">
                             <div class="col-md-9 col-lg-7">
                                 <div class="contact-wrap w-100 p-md-5 p-4">
